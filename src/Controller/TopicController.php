ad912f1cddfeb9a004858b58573091aa39584bd8
Leandro Santos <santos.leandrom@outlook.com>
2022-07-28 15:01:30 -0300

    add render topics view

--- src/Controller/TopicController.php
+++ src/Controller/TopicController.php
@@ -5,15 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Repository\TopicRepository;
+use App\Repository\PostRepository;
 
 class TopicController extends AbstractController
 {
     #[Route('topic/{id}', name: 'app_topic')]
-    public function index(int $id, TopicRepository $topicRepository)
+    public function index(int $id, PostRepository $postRepository)
     {
-        $topic = $topicRepository->findBy(['forum'=>['id'=>$id]]);
-        dd($topic);
+        $posts = $postRepository->findBy(['topic'=>['id'=>$id]]);
+        return $this->render('topic/index.html.twig',['posts'=>$posts]); 
         
     }
 }
