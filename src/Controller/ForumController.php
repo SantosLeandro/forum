907349e30d3616f5d586c40912f9861e40e1329f
santos.leandrom <santos.leandrom@outlook.com>
2022-07-30 23:12:51 -0300

    fix topics order

--- src/Controller/ForumController.php
+++ src/Controller/ForumController.php
@@ -6,12 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\TopicRepository;
+use App\Repository\PostRepository;
 class ForumController extends AbstractController
 {
     #[Route('/forum/{id}', name: 'app_forum')]
     public function index(int $id, TopicRepository $topicRepository)
     {
-        $topics = $topicRepository->findBy(['forum'=>['id'=>$id]]); 
+        $topics = $topicRepository->findBy(['forum'=>['id'=>$id]],['updatedAt'=>'DESC']); 
         return $this->render('forum/index.html.twig', ['topics'=>$topics, 'forum_id'=>$id]);
     }
 
