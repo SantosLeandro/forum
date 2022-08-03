637b36fbc08a833b69fa1e3ddfb817d2096d9145
Leandro Santos <santos.leandrom@outlook.com>
2022-08-03 16:53:30 -0300

    add user profile route

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,23 +3,22 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use App\Entity\User;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Doctrine\Persistence\ManagerRegistry;
+use App\Repository\UserRepository;
 
 class UserController extends AbstractController
 {
     #[Route('/user',methods:['GET'], name: 'app_user')]
-    public function index(): JsonResponse
+    public function index()
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UserController.php',
-        ]);
+        $user = $this->getUser();
+        $avatar_url = $this->getParameter('app.avatar_bucket_url');
+        return $this->render('/user/index.html.twig',['user'=>$user,'avatar_url'=>$avatar_url]);
     }
 
     #[Route('/login', methods:['GET','POST'], name: 'app_user_login')]
@@ -41,10 +40,13 @@ class UserController extends AbstractController
         
     }   
     
-    #[Route('user.create', methods:['GET'], name: 'app_user_create')]
+    #[Route('/user.create', methods:['GET'], name: 'app_user_create')]
     public function create()
     {
-        return $this->render('/user/create.html.twig');
+        $avatar_url = $this->getParameter('app.avatar_bucket_url');
+        $avatars = json_decode(file_get_contents($avatar_url));
+        //dd($avatars);
+        return $this->render('/user/create.html.twig',['url'=>$avatar_url,'avatars'=>$avatars->objects]);
     }
 
     #[Route('/user.store',methods:['POST'], name: 'app_user_store')]
@@ -80,6 +82,32 @@ class UserController extends AbstractController
         $entityManager->flush();
         
         return $this->render('user/login.html.twig',['message'=>'usuário criado']);
+        
+    }
+
+    
+    #[Route('/user.update',methods:['POST'], name: 'app_user_update')]
+    public function update(Request $request, UserPasswordHasherInterface $hasher, UserRepository $userRepository)
+    {
+        $content = $request->request;
+        $email = $content->get('email');
+        $plainPassword = $content->get('password');
+        $avatar = $content->get('avatar');
+
+        $user = $this->getUser();
+        
+        $user = $userRepository->findOneBy(['email'=>$user->getUserIdentifier()]); 
+        $user->setEmail($email);
+        $user->setAvatar($avatar);
+
+        if($plainPassword) {
+            $hashedPassword = $hasher->hashPassword($user, $plainPassword);
+            $user->setPassword($hashedPassword);
+        }
+
+        $userRepository->add($user, true);
+
+        return $this->redirect('/user');
         
     }
 }
